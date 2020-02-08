ffafde08430ff93008dd94d0406cbeae165cbd4d
mindset-tk <yuravian@gmail.com>
2020-02-07 21:51:28 -0800

    remove extra line breaks on !help

--- commands/help.js
+++ commands/help.js
@@ -11,8 +11,8 @@ module.exports = {
 		const { commands } = message.client;
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join('\n'));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			data.push(commands.map(command => command.name).filter(Boolean).join('\n'));
+			data.push(`You can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
 			return message.channel.send(data, { split: true });
 		}
