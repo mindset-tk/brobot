a5d346e75d40b99720be9bded82840b4277f9995
mindset-tk <yuravian@gmail.com>
2023-03-11 21:56:20 -0800

    tweaked topic change annoucne to reduce log errors

--- modules/topic.js
+++ modules/topic.js
@@ -7,22 +7,22 @@ const { isTextChannel } = require('../extras/common.js');
 exports.init = async function(client) {
   client.on('channelUpdate', async (oldChannel, newChannel) => {
     if(!isTextChannel(newChannel)) {return;}
-    const server = client.guilds.cache.get(oldChannel.guild.id);
-    const channelupdateentry = await server.fetchAuditLogs().then(auditLogs => {
+    if (oldChannel.topic != newChannel.topic) {
+      const server = client.guilds.cache.get(oldChannel.guild.id);
+      const channelupdateentry = await server.fetchAuditLogs().then(auditLogs => {
       // Find the most recent audit log that corresponds to the topic change.
       // normally this will be the very first one so check that immediately.
-      if (auditLogs.entries.first().action === 'CHANNEL_UPDATE') {
-        return auditLogs.entries.first();
-      }
-      else {
-        for(const entry of auditLogs.entries()) {
-          if (entry.action === 'CHANNEL_UPDATE') {
-            return entry;
+        if (auditLogs.entries.first().action === 'CHANNEL_UPDATE') {
+          return auditLogs.entries.first();
+        }
+        else {
+          for(const entry of auditLogs.entries()) {
+            if (entry.action === 'CHANNEL_UPDATE') {
+              return entry;
+            }
           }
         }
-      }
-    });
-    if (oldChannel.topic != newChannel.topic) {
+      });
       newChannel.send(`${channelupdateentry.executor} has changed the topic to: \n *${newChannel.topic}*`);
     }
   });
